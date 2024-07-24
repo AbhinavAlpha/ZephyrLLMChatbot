dec84e1a471fe39379a415fca413921a96352425
AbhinavAlpha <ab4370480@alphacollege.me>
2024-07-24 14:40:17 -0400

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def respond(
     temperature,
     top_p,
 ):
-    system_message = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise."
+    system_message = "You are a knowledgeable guide. You offer insightful information about the Taj Mahal, suggest key aspects to explore, and guide through understanding its historical and cultural significance. Discuss any aspect of the Taj Mahal that interests you, or ask me for a quick overview of this iconic monument. Feel free to let me know what aspect of the Taj Mahal you're curious about, or ask for a concise summary of its history and significance!"
     messages = [{"role": "system", "content": system_message}]
 
     for val in history:
@@ -30,10 +30,10 @@ def respond(
 
     for message in client.chat_completion(
         messages,
-        max_tokens=max_tokens,
+        max_tokens=1000,
         stream=True,
-        temperature=temperature,
-        top_p=top_p,
+        temperature=0.98,
+        top_p=0.7,
     ):
         token = message.choices[0].delta.content
 
@@ -46,8 +46,8 @@ For information on how to customize the ChatInterface, peruse the gradio docs: h
 demo = gr.ChatInterface(
     respond,
     additional_inputs=[
-        gr.Textbox(value = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise.", label="System message"),
-        gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
+        gr.Textbox(value = "You are a knowledgeable guide. You offer insightful information about the Taj Mahal, suggest key aspects to explore, and guide through understanding its historical and cultural significance. Discuss any aspect of the Taj Mahal that interests you, or ask me for a quick overview of this iconic monument. Feel free to let me know what aspect of the Taj Mahal you're curious about, or ask for a concise summary of its history and significance!", label="System message"),
+        gr.Slider(minimum=1, maximum=2048, value=1000, step=1, label="Max new tokens"),
         gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
         gr.Slider(
             minimum=0.1,
@@ -59,11 +59,13 @@ demo = gr.ChatInterface(
     ],
 
     examples = [ 
-        ["I feel overwhelmed with work."],
-        ["Can you guide me through a quick meditation?"],
-        ["How do I stop worrying about things I can't control?"]
+        ["I want to know about Taj Mahal."],
+        ["Who made it?"],
+        ["How many days it take to built?"]
     ],
-    title = 'Calm Mate 🕊️'
+    title = 'Taj Mahal Guide 🕌'
+
+    
 )
 
 
